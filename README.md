42b699cbd00c5b55bfef105a08eff224f097c308
Daniel Salvagni <danielsalvagni@MBP-de-Daniel.fritz.box>
2019-04-03 12:33:20 +0200

    Adds preprocessor logic and updates readme with instructions

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # karma-underscore-template-preprocessor
-Karma preprocessor for underscore templates.
+
+Enables support the underscore.js template system.
+
+# Usage
+
+```
+npm install karma-underscore-template-preprocessor --save-dev
+```
+
+karma.conf.js
+
+```
+preprocessors: {
+    "**/*.jst": ["underscoreTemplate"]
+},
+
+underscoreTemplatePreprocessor: {
+    beforeScript: "window.Namespace = window.Namespace || {}; Namespace.templates = Namespace.templates || {};",
+    globalTemplateVariable: "Namespace.templates"
+},
+```
+
+`globalTemplateVariable` will set the global variable that's used to hold your compiled templates.
+`beforeScript` will be execute before compiling each template file.
+
+
+# Result on your test files
+For a given template file locate in: `folder/templates/meeting/item.jst` > `Namespace.templates.meetingItem()`;
+For a given templatee located on template root folder: `folder/templates/item.jst` > `Namespace.templates.item()`;
---
+++ index.js
@@ -0,0 +1,79 @@
+var _, createUnderscorePreprocessor, createTemplateName, ucfirst;
+
+ucfirst = function(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+_ = require("underscore");
+
+createTemplateName = function(file, log) {
+  var path = file.originalPath,
+    folders = path.split("/"),
+    lastFolder = folders[folders.length - 2].toLowerCase(),
+    folder = lastFolder === "templates" ? "" : lastFolder,
+    fullFileName = folders[folders.length - 1],
+    fileNameParts = fullFileName.split("."),
+    fileName = folder ? ucfirst(fileNameParts[0]) : fileNameParts[0];
+
+  log.debug('Processing "%s".', folder + fileName);
+
+  return folder + fileName;
+};
+
+createUnderscorePreprocessor = function(args, config, logger) {
+  "use strict";
+
+  config = config || {};
+
+  var log = logger.create("preprocessor.underscoreTemplate"),
+    options = _.extend(args.options || {}, config.options || {}),
+    globalTemplateVariable =
+      args.globalTemplateVariable ||
+      config.globalTemplateVariable ||
+      "___templates___",
+    beforeScript = args.beforeScript || config.beforeScript || "";
+
+  return function(content, file, done) {
+    var processed = null;
+
+    var templateName = createTemplateName(file, log);
+
+    log.debug('Processing "%s".', templateName);
+
+    try {
+      processed =
+        "\
+		(function() { " +
+        beforeScript +
+        "}).call(this);(function(_) {\
+                this." +
+        globalTemplateVariable +
+        " = this." +
+        globalTemplateVariable +
+        " = this." +
+        globalTemplateVariable +
+        " || {} ;\
+                this." +
+        globalTemplateVariable +
+        "['" +
+        templateName +
+        "'] = " +
+        _.template(content).source +
+        "}).call(this, _);";
+    } catch (e) {
+      log.error("%s\n  at %s", e.message, content);
+    }
+
+    done(processed);
+  };
+};
+
+createUnderscorePreprocessor.$inject = [
+  "args",
+  "config.underscoreTemplatePreprocessor",
+  "logger"
+];
+
+module.exports = {
+  "preprocessor:underscoreTemplate": ["factory", createUnderscorePreprocessor]
+};
